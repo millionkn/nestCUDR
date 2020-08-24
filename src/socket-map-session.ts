189a041fca7c50e00988a1dd9897ca1fcb23cbee
millionkn <millionkn@sina.com>
2020-08-24 11:21:13 +0900

    bindToken之后会自动刷新session

--- src/socket-map-session.ts
+++ src/socket-map-session.ts
@@ -3,6 +3,7 @@ import { sessionStore } from "src/sessionStore";
 
 const socketStoreSession = new Map<string, string>()
 const tokenStoreSocket = new Map<string, string>();
+const unbindHandler = new Map<string, () => void>();
 
 export namespace SocketMapSession {
   export async function getSession(socket: Socket) {
@@ -16,11 +17,20 @@ export namespace SocketMapSession {
     const socketId = tokenStoreSocket.get(body.token);
     if (!socketId) { return null }
     socketStoreSession.set(socketId, session.id);
+    const timmer = setInterval(() => session.touch(), 1 * 60 * 1000);
+    unbindHandler.set(socketId, () => {
+      clearInterval(timmer);
+      unbindHandler.delete(socketId)
+    })
     return true;
   }
   export async function createToken(socket: Socket) {
     const token = `#${Math.random()}${Math.random()}${Math.random()}`
     tokenStoreSocket.set(token, socket.id);
     return { token };
+  }
+  export function unbind(socket: Socket) {
+    const handler = unbindHandler.get(socket.id);
+    if (handler) { handler() }
   }
 }
