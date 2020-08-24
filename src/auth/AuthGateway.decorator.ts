32eb31c06897cd02b6c78a61216019fc2d916179
millionkn <millionkn@sina.com>
2020-08-24 12:09:05 +0900

    AuthGateway

---
+++ src/auth/AuthGateway.decorator.ts
@@ -0,0 +1,32 @@
+import { Type } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { SocketMapSession } from 'src/socket-map-session';
+import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+
+function notDelete(x: any) { }
+
+export function AuthGateway() {
+  return (klass: Type<any>) => {
+    const subklassName = `${Math.random()}`.replace('0.', 'Gateway');
+    class Gateway extends klass implements OnGatewayConnection, OnGatewayDisconnect {
+      async handleConnection(client: Socket) {
+        const token = await SocketMapSession.createToken(client)
+        await new Promise((res) => client.emit('auth', token, res));
+        const handler = super.handleConnection || function () { };
+        return await handler(client);
+      }
+      async handleDisconnect(client: Socket) {
+        const handler = super.handleDisconnect || function () { };
+        try {
+          return await handler(client);
+        } catch (e) {
+          throw e
+        } finally {
+          SocketMapSession.unbind(client);
+        }
+      }
+    }
+    notDelete(Gateway);
+    return eval(`class ${subklassName} extends Gateway{};${subklassName}`);
+  }
+}
--- src/business/business.gateway.ts
+++ src/business/business.gateway.ts
@@ -1,18 +1,17 @@
 import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { CurrentUser } from 'src/auth/current-user.decorator';
-import { SocketMapSession } from 'src/socket-map-session';
+import { AuthGateway } from 'src/auth/AuthGateway.decorator';
 
 @WebSocketGateway()
+@AuthGateway()
 export class BusinessGateway implements OnGatewayConnection,OnGatewayDisconnect {
   @SubscribeMessage('getUser')
   async getUser(socket: Socket, payload: any, @CurrentUser() user: any) {
     return user;
   }
   async handleConnection(client: Socket) {
-    client.emit('auth', await SocketMapSession.createToken(client));
   }
   handleDisconnect(client: Socket) {
-    SocketMapSession.unbind(client);
   }
 }
