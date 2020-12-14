2f2c573bddcc948609ff18595003225e5b62085a
millionkn <millionkn@sina.com>
2020-12-14 19:02:51 +0900

    tttt

--- src/cudr/tableQuery2.ts
+++ src/cudr/tableQuery2.ts
@@ -40,14 +40,7 @@ interface Column<T, cudrNull extends boolean, cudrArray extends boolean> {
   [columnSym]: WrapperType<T, cudrNull, cudrArray>
 }
 
-interface ArrayHandlers<B extends TableQueryBody<any>> extends Column<QueryResult<B>, false, true> {
-  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, false>): {
-    filter(filter: Filter<T>): ArrayHandlers<B>
-    sort(mode: 'desc' | 'asc' | undefined | null): ArrayHandlers<B>
-  }
-  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, true>): {
-    filter: (mode: 'isEmpty' | 'notEmpty' | undefined | null) => ArrayHandlers<B>
-  }
+interface JoinWhat<B extends TableQueryBody<any>> extends Column<QueryResult<B>, false, true> {
   count(): Column<number, false, false>
   count(path: (resultColumns: QueryResultColumns<B>) => Column<any, false, false>): Column<number, false, false>
   sum(path: (resultColumns: QueryResultColumns<B>) => Column<number, false, false>): Column<number, false, false>
@@ -63,10 +56,8 @@ interface QueryFuns<E extends CudrBaseEntity> {
     setNullAs(nullValue: T): Column<T, false, false>
   }
   join<T extends CudrBaseEntity, B extends TableQueryBody<T>>(
-    joinTarget: Type<T>,
-    otherSide: (target: Wrapper<T, false, false>) => Wrapper<E, boolean, boolean>,
-    body: B,
-  ): ArrayHandlers<B>
+    subQuery:SubTableQuery<T,B,E>
+  ): JoinWhat<B>
 }
 
 type TableQueryBody<E extends CudrBaseEntity<any>> = {
@@ -90,45 +81,45 @@ type QueryResult<B extends TableQueryBody<any>> = {
   : never
 }
 
-interface TableQueryBuilder<B extends TableQueryBody<any>> {
-  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, false>): {
-    filter(filter: Filter<T>): TableQueryBuilder<B>
-    sort(mode: 'desc' | 'asc' | undefined | null): TableQueryBuilder<B>
-  }
-  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, true>): {
-    filter: (mode: 'isEmpty' | 'notEmpty' | undefined | null) => TableQueryBuilder<B>
-  }
-  query(opts?: {
-    manager?: EntityManager,
-    page?: {
-      index: number,
-      size: number,
-    }
-  }): Promise<QueryResult<B>[]>;
+const SubTableQueryKlassSym = Symbol();
+const SubTableQueryBodySym = Symbol();
+const SubTableQueryOutTypeSym = Symbol();
+type SubTableQuery<E extends CudrBaseEntity, B extends TableQueryBody<E>, O extends CudrBaseEntity> = {
+  [SubTableQueryKlassSym]: E
+  [SubTableQueryBodySym]: B
+  [SubTableQueryOutTypeSym]: O
 }
 
-export function tableQuery<E extends CudrBaseEntity, B extends TableQueryBody<E>>(klass: Type<E>, body: B): TableQueryBuilder<B> {
+interface TableQueryBuilder<E extends CudrBaseEntity, B extends TableQueryBody<E>> {
+  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, false>): {
+    filter(filter: Filter<T>): TableQueryBuilder<E, B>
+    sort(mode: 'desc' | 'asc' | undefined | null): TableQueryBuilder<E, B>
+  }
+  column<T>(path: (resultColumns: QueryResultColumns<B>) => Column<T, false, true>): {
+    filter: (mode: 'isEmpty' | 'notEmpty' | undefined | null) => TableQueryBuilder<E, B>
+  }
+  query(manager?: EntityManager, opts?: {
+    skip?: number,
+    take?: number,
+  }): Promise<QueryResult<B>[]>;
+  asSubQuery<T extends CudrBaseEntity>(joinPath: (e: Wrapper<E, false, false>) => Wrapper<T, any, false>): SubTableQuery<E, B, T>
+}
+
+export function tableQuery<E extends CudrBaseEntity, B extends TableQueryBody<E>>(klass: Type<E>, body: B): TableQueryBuilder<E, B> {
   throw new Error();
 }
+
+const r1 = tableQuery(UserRequirementEntity, {
+  num: ({ ref }) => ref(e => e.test)
+}).column((e) => e.num).filter({ lessOrEqual: 1 }).asSubQuery((e)=>e.user);
+
 tableQuery(UserEntity, {
   id: ({ ref }) => ref((e) => e.id),
   userEntity: ({ ref }) => ref((e) => e),
-  requirementsArrayCount: ({ join }) => join(UserRequirementEntity, (e) => e.user, {
-  }).count(),
-  requirementsTestSum: ({ join }) => join(UserRequirementEntity, (e) => e.user, {
-    num: ({ ref }) => ref(e => e.test)
-  })
-    .column((e) => e.num).filter({ lessOrEqual: 1 })
-    .sum(e => e.num),
+  requirementsArrayCount: ({ join }) => join(r1).count(),
+  requirementsTestSum: ({ join }) => join(r1).sum(e => e.num),
   name: ({ ref }) => ref(e => e.name),
 })
   .column((e) => e.userEntity).filter({ in: [] })
   .column((e) => e.requirementsArrayCount).filter({ moreOrEqual: 1 })
   .column((e) => e.name).sort('desc')
-  .query()
-  .then(([result]) => {
-    const r1: number = result.userEntity.num23;
-    const r2: UserEntity['id'] = result.id;
-    const r3: string = result.name;
-    const r4: number = result.requirementsArrayCount;
-  })
