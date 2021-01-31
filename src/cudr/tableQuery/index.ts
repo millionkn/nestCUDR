c4d4f1e6cf3feb0d9485a45b0bc0098088f4d7a9
millionkn <millionkn@sina.com>
2021-01-31 19:49:04 +0800

    ppp

--- src/cudr/tableQuery/index.ts
+++ src/cudr/tableQuery/index.ts
@@ -13,27 +13,25 @@ export interface ID<T = any> {
 }
 
 const hideSym = Symbol();
-type Hide<need extends boolean, name extends string, T> = {
-  [hideSym]: need extends true ? {
-    [key in name]: T
-  } : {
-    [key in name]?: T
-  }
+type Hide<need extends boolean, T> = {
+  [hideSym]: need extends true ? Required<T> : Partial<T>
 }
 
-type BaseWrapper<T, isNull extends boolean, isArray extends boolean> = Hide<false, 'wrapper', {
-  type: T,
-  isArray: isArray,
-  isNull: isNull,
+type BaseWrapper<T, isNull extends boolean, isArray extends boolean> = Hide<false, {
+  wrapper: {
+    type: T,
+    isArray: isArray,
+    isNull: isNull,
+  }
 }>
 
-type Wrapper<T, isNull extends boolean, isArray extends boolean> = T extends BaseEntityKlass ? {
-  [key in keyof T]
+type Wrapper<T, isNull extends boolean, isArray extends boolean> = T extends BaseEntityKlass<any> ? {
+  [key in keyof Required<T>]
   : T[key] extends Function ? never
-  : T[key] extends BaseEntityKlass ? Wrapper<T[key], isNull, isArray>
+  : T[key] extends BaseEntityKlass<any> ? Wrapper<T[key], isNull, isArray>
   : T[key] extends Array<infer X> ? Wrapper<X, isNull, true>
-  : T[key] extends null | undefined | infer X ? Wrapper<X, true, isArray>
-  : BaseWrapper<T[key], isNull, isArray>
+  : T[key] extends null | undefined | infer X ? Wrapper<X, null extends T[key] ? true : undefined extends T[key] ? true : isNull, isArray>
+  : never
 } : BaseWrapper<T, isNull, isArray>
 
 type Filter<T> = null | undefined | (T extends Date ? { lessOrEqual: Date } | { moreOrEqual: Date } | { lessOrEqual: Date, moreOrEqual: Date } | {}
@@ -43,14 +41,14 @@ type Filter<T> = null | undefined | (T extends Date ? { lessOrEqual: Date } | { 
   : T extends ID ? { equal: T } | { in: T[] } | {}
   : {});
 
-type QueryTableBody<Entity extends BaseEntityKlass> = {
+type QueryTableBody<Entity extends BaseEntityKlass<any>> = {
   [key: string]: ColumnPoint<Entity, any, boolean, boolean>
 }
 
-type TKeys<Entity extends BaseEntityKlass> = {
+type TKeys<Entity extends BaseEntityKlass<any>> = {
   [key in keyof Entity]: Entity[key] extends null | undefined | infer X
-  ? X extends BaseEntityKlass ? never
-  : X extends BaseEntityKlass[] ? never
+  ? X extends BaseEntityKlass<any> ? never
+  : X extends BaseEntityKlass<any>[] ? never
   : key
   : never
 }[keyof Entity]
@@ -58,34 +56,38 @@ type TKeys<Entity extends BaseEntityKlass> = {
 /**
  * Pick 可以保留是method还是property,原因未知
  */
-type Simple<T> = T extends BaseEntityKlass ? Pick<T, TKeys<T>> : T
+type Simple<T> = T extends BaseEntityKlass<any> ? Pick<T, TKeys<T>> : T
 
 type Cover<T, isNull, isArray> = isArray extends true ? Simple<T>[] : isNull extends true ? Simple<T> | null : Simple<T>
 
-type QueryResult<Entity extends BaseEntityKlass, Body1 extends QueryTableBody<Entity>> = {
+type QueryResult<Entity extends BaseEntityKlass<any>, Body1 extends QueryTableBody<Entity>> = {
   [key in keyof Body1]: Body1[key] extends ColumnPoint<Entity, infer Type, infer isNull, infer isArray> ? Cover<Type, isNull, isArray> : never
 }
 
 
-type ColumnPointFuns<Entity extends BaseEntityKlass> = {
+type ColumnPointFuns<Entity extends BaseEntityKlass<any>> = {
   ref<T, isNull extends boolean>(
     path: (entity: Wrapper<Entity, false, false>) => Wrapper<T, isNull, false>
-  ): ColumnPoint<Entity, T, isNull, false>
-  join<E extends BaseEntityKlass, Body2 extends QueryTableBody<E>>(
+  ): ColumnPoint<Entity, T, isNull, false> & Hide<true, {
+    path: (entity: Wrapper<Entity, false, false>) => Wrapper<any, boolean, false>
+  }>,
+  join<E extends BaseEntityKlass<any>, Body2 extends QueryTableBody<E>>(
     extraTable: ExtraTable<E, Body2, Entity>
   ): ColumnPoint<Entity, QueryResult<E, Body2>, false, true> & {
     count(opt?: {
-      path?: (body: Body2) => ColumnPoint<E, any, boolean, boolean>,
+      column?: (body: Body2) => ColumnPoint<E, any, boolean, false> & Hide<true, {
+        path: (entity: Wrapper<E, false, false>) => Wrapper<any, boolean, false>
+      }>,
       distinct?: boolean,
     }): ColumnPoint<Entity, number, false, false>,
     sum(
-      path: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
+      column: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
     ): ColumnPoint<Entity, number, false, false>,
     max(
-      path: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
+      column: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
     ): ColumnPoint<Entity, number, false, false>,
     min(
-      path: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
+      column: (body: Body2) => ColumnPoint<E, number, boolean, boolean>,
     ): ColumnPoint<Entity, number, false, false>,
     slice(
       skip: number,
@@ -94,24 +96,29 @@ type ColumnPointFuns<Entity extends BaseEntityKlass> = {
   }
 }
 
-export type ColumnPoint<Entity extends BaseEntityKlass, T, isNull extends boolean, isArray extends boolean> = Hide<false, 'columnPoint', {
-  type: T,
-  isNull: isNull,
-  isArray: isArray,
-}> & Hide<true, 'columnFuns', {
-  factory: (aliasName: string, property: string | symbol, tqb: TableQueryBuilder<Entity>) => void
+export type ColumnPoint<Entity extends BaseEntityKlass<any>, T, isNull extends boolean, isArray extends boolean> = Hide<false, {
+  columnPoint: {
+    type: T,
+    isNull: isNull,
+    isArray: isArray,
+  }
+}> & Hide<true, {
+  columnFactory: (aliasName: string, property: string | symbol, tqb: TableQueryBuilder<Entity>) => void
 }>
 
-type ExtraTable<Entity extends BaseEntityKlass, Body3 extends QueryTableBody<Entity>, JoinTarget extends BaseEntityKlass> = Hide<false, 'extraTable', {
-  Entity: Entity,
-  JoinTarget: JoinTarget,
-}> & Hide<true, 'extraTableBody', Body3>
+type ExtraTable<Entity extends BaseEntityKlass<any>, Body3 extends QueryTableBody<Entity>, JoinTarget extends BaseEntityKlass<any>> = Hide<true, {
+  extraTable: {
+    Entity?: Entity,
+    JoinTarget?: JoinTarget,
+    body: Body3,
+  }
+}>
 
-type TableTemplate<Entity extends BaseEntityKlass, Body4 extends QueryTableBody<Entity>> = {
+type TableTemplate<Entity extends BaseEntityKlass<any>, Body4 extends QueryTableBody<Entity>> = {
   [key in Extract<keyof Body4, string>]: (funs: ColumnPointFuns<Entity>) => Body4[key]
 }
 
-type TableBuilder<Entity extends BaseEntityKlass, Body5 extends QueryTableBody<Entity>> = {
+type TableBuilder<Entity extends BaseEntityKlass<any>, Body5 extends QueryTableBody<Entity>> = {
   byProperty<T, isNull extends boolean>(path: (body: Body5) => ColumnPoint<Entity, T, isNull, false>): {
     filter(filter: Filter<T>): {
       assert(value: 'notNull' | 'allowNull' | 'isNull'): TableBuilder<Entity, Body5>
@@ -125,12 +132,13 @@ type TableBuilder<Entity extends BaseEntityKlass, Body5 extends QueryTableBody<E
     take?: number,
   }): Promise<QueryResult<Entity, Body5>[]>;
   count(manager: EntityManager): Promise<number>;
-  asExtra<E extends BaseEntityKlass>(
+  asExtra<E extends BaseEntityKlass<any>>(
+    klass: Type<E>,
     joinPath: (e: Wrapper<Entity, false, false>) => Wrapper<E, boolean, false>,
   ): ExtraTable<Entity, Body5, E>
 }
 
-class TableQueryBuilder<Entity extends BaseEntityKlass>{
+class TableQueryBuilder<Entity extends BaseEntityKlass<any>>{
   constructor(
     private manager: EntityManager,
     private klass: Type<Entity>,
@@ -142,14 +150,14 @@ class TableQueryBuilder<Entity extends BaseEntityKlass>{
   ) {
     throw new TODOError();
   }
-  addExtra<E extends BaseEntityKlass, Body6 extends QueryTableBody<E>>(
+  addExtra<E extends BaseEntityKlass<any>, Body6 extends QueryTableBody<E>>(
     alias: string,
     extra: ExtraTable<E, Body6, Entity>,
     cb: (output: any, value: QueryResult<E, Body6>[]) => void,
   ) {
     throw new TODOError();
   }
-  addSlice<E extends BaseEntityKlass, Body7 extends QueryTableBody<E>>(
+  addSlice<E extends BaseEntityKlass<any>, Body7 extends QueryTableBody<E>>(
     alias: string,
     extra: ExtraTable<E, Body7, Entity>,
     opts: { skip: number, take: number },
@@ -157,30 +165,34 @@ class TableQueryBuilder<Entity extends BaseEntityKlass>{
   ): void {
     throw new TODOError();
   }
-  addMax<T, isNull extends boolean, isArray extends boolean>(
+  addMax<E extends BaseEntityKlass<any>, Body8 extends QueryTableBody<E>, T, isNull extends boolean, isArray extends boolean>(
     alias: string,
-    path: (wrapper: Wrapper<Entity, false, false>) => Wrapper<T, isNull, isArray>,
+    extra: ExtraTable<E, Body8, Entity>,
+    path: (body: Body8) => ColumnPoint<E, T, isNull, isArray>,
     cb: (output: any, value: Cover<T, isNull, isArray>) => void,
   ) {
     throw new TODOError();
   }
-  addMin<T, isNull extends boolean, isArray extends boolean>(
+  addMin<E extends BaseEntityKlass<any>, Body8 extends QueryTableBody<E>, T, isNull extends boolean, isArray extends boolean>(
     alias: string,
-    path: (wrapper: Wrapper<Entity, false, false>) => Wrapper<T, isNull, isArray>,
+    extra: ExtraTable<E, Body8, Entity>,
+    path: (body: Body8) => ColumnPoint<E, T, isNull, isArray>,
     cb: (output: any, value: Cover<T, isNull, isArray>) => void,
   ) {
     throw new TODOError();
   }
-  addSum<T, isNull extends boolean, isArray extends boolean>(
+  addSum<E extends BaseEntityKlass<any>, Body8 extends QueryTableBody<E>, T, isNull extends boolean, isArray extends boolean>(
     alias: string,
-    path: (wrapper: Wrapper<Entity, false, false>) => Wrapper<T, isNull, isArray>,
+    extra: ExtraTable<E, Body8, Entity>,
+    path: (body: Body8) => ColumnPoint<E, T, isNull, isArray>,
     cb: (output: any, value: Cover<T, isNull, isArray>) => void,
   ) {
     throw new TODOError();
   }
-  addCount<T, isNull extends boolean, isArray extends boolean>(
+  addCount<E extends BaseEntityKlass<any>, Body8 extends QueryTableBody<E>, T, isNull extends boolean, isArray extends boolean>(
     alias: string,
-    path: (wrapper: Wrapper<Entity, false, false>) => Wrapper<T, isNull, isArray>,
+    extra: ExtraTable<E, Body8, Entity>,
+    path: (body: Body8) => ColumnPoint<E, T, isNull, isArray>,
     cb: (output: any, value: Cover<T, isNull, isArray>) => void,
   ) {
     throw new TODOError();
@@ -196,7 +208,7 @@ class TableQueryBuilder<Entity extends BaseEntityKlass>{
   }
 }
 
-function resolveTemplate<Entity extends BaseEntityKlass, Table extends QueryTableBody<Entity>>(tableTemplate: TableTemplate<Entity, Table>): Table {
+function resolveTemplate<Entity extends BaseEntityKlass<any>, Table extends QueryTableBody<Entity>>(tableTemplate: TableTemplate<Entity, Table>): Table {
   const ret: any = {};
   for (const k in tableTemplate) {
     if (tableTemplate.hasOwnProperty(k)) {
@@ -204,11 +216,11 @@ function resolveTemplate<Entity extends BaseEntityKlass, Table extends QueryTabl
         ref(path) {
           return {
             [hideSym]: {
-              columnFuns: {
-                factory(alias, property, tqb) {
-                  tqb.addPath(alias, path, (output, value) => output[property] = value);
-                },
-              }
+              path,
+              columnFactory(alias, property, tqb) {
+                tqb.addPath(alias, path, (output, value) => output[property] = value);
+              },
+
             }
           }
         },
@@ -216,34 +228,30 @@ function resolveTemplate<Entity extends BaseEntityKlass, Table extends QueryTabl
           const extraTable = niet;
           return {
             [hideSym]: {
-              columnFuns: {
-                factory(alias, prototype, tqb) {
-                  tqb.addExtra(alias, extraTable, (output, value) => output[prototype] = value)
-                }
+              columnFactory(alias, prototype, tqb) {
+                tqb.addExtra(alias, extraTable, (output, value) => output[prototype] = value)
               }
             },
             count(opt) {
               return {
                 [hideSym]: {
-                  columnFuns: {
-                    factory(alias, prototype, tqb) {
-                      throw new TODOError();
-                      // if (opt?.path) {
-                      //   opt.path(extraTable[hideSym].extraTableBody)
-                      // }
-                      // tqb.addExtra(alias, extraTable, (output, value) => output[prototype] = value)
+                  columnFactory(alias, prototype, tqb) {
+                    if (opt?.column) {
+                      tqb.addCount(alias, extraTable, opt.column, (output, value) => output[prototype] = value)
+                    } else {
+                      tqb.addCount(alias, extraTable, (w) => w, (output, value) => output[prototype] = value)
                     }
                   }
                 }
               }
             },
-            sum(path) {
+            sum(columnPath) {
               throw new TODOError();
             },
-            max(path) {
+            max(columnPath) {
               throw new TODOError();
             },
-            min(path) {
+            min(columnPath) {
               throw new TODOError();
             },
             slice(skip, take) {
@@ -257,21 +265,21 @@ function resolveTemplate<Entity extends BaseEntityKlass, Table extends QueryTabl
   return ret;
 }
 
-export function tableQuery<
-  Entity extends BaseEntityKlass,
-  Table extends QueryTableBody<Entity>,
+function subTableQuery<
+  Entity extends BaseEntityKlass<any>,
+  TableBody extends QueryTableBody<Entity>,
   >(
     klass: Type<Entity>,
-    tableTemplate: TableTemplate<Entity, Table>,
+    tableBody: TableBody,
     factory: (tqb: TableQueryBuilder<Entity>) => void,
-): TableBuilder<Entity, Table> {
+): TableBuilder<Entity, TableBody> {
   return {
     byProperty(path) {
       return {
         filter(filter) {
           return {
             assert(nullType) {
-              return tableQuery(klass, tableTemplate, (tqb) => {
+              return subTableQuery(klass, tableBody, (tqb) => {
                 throw new TODOError()
               });
             }
@@ -280,7 +288,7 @@ export function tableQuery<
         sort(sortMode) {
           return {
             setNullOn(nullMode) {
-              return tableQuery(klass, tableTemplate, (tqb) => {
+              return subTableQuery(klass, tableBody, (tqb) => {
                 throw new TODOError()
               });
             }
@@ -293,23 +301,32 @@ export function tableQuery<
     },
     async query(manager, opts) {
       const tableQueryBuilder = new TableQueryBuilder(manager, klass);
-      const table = resolveTemplate(tableTemplate);
-      Object.keys(table).forEach((key, index) => {
-        const element = table[key][hideSym];
-        element.columnFuns.factory(`body_${index}`, key, tableQueryBuilder);
+      Object.keys(tableBody).forEach((key, index) => {
+        const element = tableBody[key][hideSym];
+        element.columnFactory(`body_${index}`, key, tableQueryBuilder);
       });
       factory(tableQueryBuilder);
       return await tableQueryBuilder.query(opts);
     },
     async count(manager) {
       const tableQueryBuilder = new TableQueryBuilder(manager, klass);
-      const table = resolveTemplate(tableTemplate);
-      Object.keys(table).forEach((key, index) => {
-        const element = table[key][hideSym];
-        element.columnFuns.factory(`body_${index}`, key, tableQueryBuilder);
+      Object.keys(tableBody).forEach((key, index) => {
+        const element = tableBody[key][hideSym];
+        element.columnFactory(`body_${index}`, key, tableQueryBuilder);
       });
       factory(tableQueryBuilder);
       return await tableQueryBuilder.count();
     }
   }
+}
+
+export function tableQuery<
+  Entity extends BaseEntityKlass<any>,
+  Template extends TableTemplate<Entity, QueryTableBody<Entity>>,
+  >(
+    klass: Type<Entity>,
+    template: Template
+  ) {
+  const tableBody = resolveTemplate(template);
+  return subTableQuery(klass, tableBody, () => { }) as TableBuilder<Entity, { [key in keyof Template]: ReturnType<Template[key]> }>;
 }
---
+++ src/cudr/tableQuery/test.ts
@@ -0,0 +1,46 @@
+import { tableQuery } from "./";
+import { BaseEntityKlass } from "./BaseEntityKlass";
+
+class A extends BaseEntityKlass<'A'>{
+  aName!: string
+  aNum!: number
+  aSuperB!: B
+  aExtracC!: C[]
+  aSuperD?: D;
+}
+
+class B extends BaseEntityKlass<'B'>{
+  bName!: string
+  bNum!: number
+}
+class C extends BaseEntityKlass<'C'>{
+  cName?: string
+  cNum!: number
+  cSuperaA!: A;
+}
+class D extends BaseEntityKlass<'D'>{
+  dName!: string
+  dNum!: number
+}
+
+const extra = tableQuery(C, {
+  cName: ({ ref }) => ref((w) => w.cName)
+}).asExtra(A, (e) => e.cSuperaA)
+
+const builderA = tableQuery(A, {
+  dName: ({ ref }) => ref((w) => w.aSuperD.dName),
+  aNum: ({ ref }) => ref((w) => w.aNum),
+  cExtra: ({ join }) => join(extra),
+  dCount: ({ join }) => join(extra).count({
+    column: (w) => w.cName,
+    distinct: true,
+  }),
+}).query({} as any, {} as any).then(([result]) => {
+  result.dName
+  result.aNum
+  const [c] = result.cExtra;
+
+
+
+  c.cName
+})
